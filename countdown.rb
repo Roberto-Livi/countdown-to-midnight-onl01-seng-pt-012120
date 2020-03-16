ae634b546879bd4ac6b6cca1f9225bc0d9a7d36d
Roberto Livi <robertomlivi@outlook.com>
2020-03-16 03:11:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+def countdown(number)
+  while number != 0
+    "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number != 0
+    "#{number} SECOND(S)!"
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
